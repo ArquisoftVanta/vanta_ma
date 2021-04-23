a203013a2d67fe479dcef520fb43c777110a534a
Mauricio Meza <mmezab@unal.edu.co>
2021-04-23 17:04:35 -0500

    final app navigation

--- app/src/main/java/com/dragonfly/vanta/MainActivity.java
+++ app/src/main/java/com/dragonfly/vanta/MainActivity.java
@@ -1,16 +1,12 @@
 package com.dragonfly.vanta;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.ui.NavigationUI;
@@ -20,8 +16,10 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
+    Toolbar toolbar;
     NavigationView navigationView;
     NavController navController;
+    ActionBarDrawerToggle drawerToggle;
 
 
     @Override
@@ -31,25 +29,16 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
-        //navigationView.setNavigationItemSelectedListener(this);
+        toolbar = findViewById(R.id.toolbar);
 
+        this.setSupportActionBar(toolbar);
         navController = Navigation.findNavController(this, R.id.nav_fragment);
         NavigationUI.setupWithNavController(navigationView, navController);
-    }
 
-    /*
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.mainMenu:
-                break;
-            case R.id.profileMenu:
-                break;
-            case R.id.autoMenu:
-                break;
-        }
-        return true;
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.abrir, R.string.cerrar);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
     }
-    */
 
 }
