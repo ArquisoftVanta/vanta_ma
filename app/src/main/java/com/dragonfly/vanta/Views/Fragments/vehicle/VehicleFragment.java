739eaecadf1acd1228fe84631582cc7ed8381ce9
Sebastian Reina <semorenore@unal.edu.co>
2021-04-26 18:14:21 -0500

    1.0

--- app/src/main/java/com/dragonfly/vanta/Views/Fragments/vehicle/VehicleFragment.java
+++ app/src/main/java/com/dragonfly/vanta/Views/Fragments/vehicle/VehicleFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dragonfly.vanta.R;
 
@@ -26,5 +27,8 @@ public class VehicleFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+
     }
 }
