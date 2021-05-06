577549d09834b63f754043f11f9935f84a136fc1
Cristian Sainea <cesaineam@unal.edu.co>
2021-05-05 22:21:39 -0500

    nuevo viaje

--- app/src/main/java/com/dragonfly/vanta/Views/Fragments/service/NewPostFragment.java
+++ app/src/main/java/com/dragonfly/vanta/Views/Fragments/service/NewPostFragment.java
@@ -21,6 +21,7 @@ import com.dragonfly.vanta.MainActivity;
 import com.dragonfly.vanta.R;
 import com.dragonfly.vanta.ViewModels.RequestViewModel;
 import com.google.android.gms.common.api.Status;
+
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
--- app/src/main/java/com/dragonfly/vanta/Views/Fragments/service/NewServiceFragment.java
+++ app/src/main/java/com/dragonfly/vanta/Views/Fragments/service/NewServiceFragment.java
@@ -1,21 +1,27 @@
 package com.dragonfly.vanta.Views.Fragments.service;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.dragonfly.vanta.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class NewServiceFragment extends Fragment {
-    private CoordinatesInput coorOrg, coorDst;
-    private RequestInput requestInput;
-    private String mail;
+    AutocompleteSupportFragment orgFragment, dstFragment;
+
 
 
     public static NewServiceFragment newInstance() { return new NewServiceFragment(); }
@@ -30,5 +36,42 @@ public class NewServiceFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initializeGooglePlaces();
+    }
+    private void initializeGooglePlaces() {
+        List<Place.Field> places = Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG);
+
+        if(!Places.isInitialized()){
+            Places.initialize(getContext(), "AIzaSyAxm0QLs59dJ34JezS4XmSs75bHKrFUBz0");
+        }
+
+        orgFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment_org_ser);
+        orgFragment.setPlaceFields(places);
+        orgFragment.setCountry("CO");
+        orgFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+
+            }
+            @Override
+            public void onError(@NonNull Status status) {
+            }
+        });
+
+        dstFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment_dst_ser);
+        dstFragment.setPlaceFields(places);
+        dstFragment.setCountry("CO");
+        dstFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+
+            }
+            @Override
+            public void onError(@NonNull Status status) {
+            }
+        });
+
     }
 }
